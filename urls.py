05aae436e0bbaf43c824c5da3343d071c40eed1b
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2026-06-07 00:51:01 +0500

    URLS for scrapping

---
+++ urls.py
@@ -0,0 +1,13 @@
+DEV_URLS = [
+  "https://fastapi.tiangolo.com/",
+    "https://docs.python.org/3/tutorial/",
+]
+
+ML_URLS = [
+    "https://en.wikipedia.org/wiki/Machine_learning",
+    "https://en.wikipedia.org/wiki/Overfitting",
+]
+
+GENERAL_URLS = [
+    "https://en.wikipedia.org/wiki/Artificial_intelligence"
+]
